a5af94bbab914e5df1158f983e8c39a76cfc37f2
Tyreeze <wedding5@msu.edu>
2022-03-09 11:42:02 -0600

    set up both scores pages

---
+++ assets/js/scores.js
@@ -0,0 +1,50 @@
+    function renderHighScores() {
+        let highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+        highscores.sort(function(a, b) {
+            return b.score - a.score;
+        });
+
+        highscores.forEach(function(score) {
+            let listTag = document.createElement("list");
+            listTag.textContent = score.initials + " - " + score.score;
+
+            let liEl = document.getElementById("highscores");
+            liEl.appendChild(listTag);
+        });
+    }
+
+    function clearHighScores() {
+        window.localStorage.removeItem("highscores");
+        window.location.reload();
+    }
+
+
+    function saveHighScores() {
+        let initials = initialsEl.value.trim();
+
+        if (initials !== "") {
+            let highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+            let newScore = {
+                score: time,
+                initials: initials
+            };
+
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+
+        window.location.href = "highscores.html";
+        }
+    }
+
+    function searchForClick(e) {
+        if (e.key === "Enter") {
+            saveHighScores();
+        }
+    }
+
+    initialsEl.onkeyup = searchForClick;
+
+    document.getElementById("clear").onClick = clearHighScores;
+
+    renderHighScores();

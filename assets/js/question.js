4ce5feffea9a74b9c01ab955a1f883a465e09969
Tyreeze <wedding5@msu.edu>
2022-03-09 01:01:27 -0600

    questions are created

---
+++ assets/js/question.js
@@ -0,0 +1,38 @@
+let questions = [
+    {
+        title:"Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer:"alerts"
+    },
+    {
+        title:"Arrays in JavaScript can be used to store ___.",
+        choices:["numbers and strings", "other arrays", "booleans", "all of the abover"],
+        answer:"all of the above"
+    },
+    {
+        title: "Which of the following function of String object combines the text of two strings and returns a new string?",
+        choices: ["add()", "concat()", " merge()", "append()"],
+        answer: "concat()"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        title: "Which of the following function of an array object adds and/or removes elements from an array?",
+        choices: ["toSource()", "sort()", "unshift()", "splice()"],
+        answer: "splice()"
+    },
+    {
+        title: "Which of the following function of Array object joins all elements of an array into a string?",
+        choices: ["concat()", "join()", "pop()", "map()"],
+        answer: "join()"
+    },
+    {
+        title:"A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["console.table", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log"
+      }
+
+];
